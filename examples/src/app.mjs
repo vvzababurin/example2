e778009baff7b6c00e8dc2b889aedf6e4d527513
vvzababurin <vvzababurin@gmail.com>
2024-07-02 09:27:20 +0300

    update

--- examples/src/app.mjs
+++ examples/src/app.mjs
@@ -1,79 +1,79 @@
 try {
-    window["isPlaybackInProgress"] = false;
+    globalThis["isPlaybackInProgress"] = false;
 
-    window["startplayback"] = async function()
+    globalThis["startplayback"] = async function()
     {
-		window["isPlaybackInProgress"] = true;
+		globalThis["isPlaybackInProgress"] = true;
 
-		if (window["audioCtx"] == undefined) {
-			window["audioCtx"] = new (window.AudioContext || window.webkitAudioContext)();
+		if (globalThis["audioCtx"] == undefined) {
+			globalThis["audioCtx"] = new (globalThis.AudioContext || globalThis.webkitAudioContext)();
 		}
 
-		await window["audioCtx"].audioWorklet.addModule("./nk-radio/modules/radio/radio-processor.mjs");
+		await globalThis["audioCtx"].audioWorklet.addModule("./nk-radio/modules/radio/radio-processor.mjs");
 		 
-		window["audioCtx"].suspend();
+		globalThis["audioCtx"].suspend();
 
 		const audioSrc = "https://hermitage.hostingradio.ru/hermitage128.mp3";
 	
 		$("#audiostream").attr("src", audioSrc);
-		window["audioElement"] = $("#audiostream")[0];
+		globalThis["audioElement"] = $("#audiostream")[0];
 	
-		if (window["audioSrc"] == undefined) {
-			window["audioSrc"] = window["audioCtx"].createMediaElementSource(window["audioElement"]);
+		if (globalThis["audioSrc"] == undefined) {
+			globalThis["audioSrc"] = globalThis["audioCtx"].createMediaElementSource(globalThis["audioElement"]);
 		}
 
-		if (window["audioSrc"]) {
-			window["audioSrc"].connect(window["audioCtx"].destination);
+		if (globalThis["audioSrc"]) {
+			globalThis["audioSrc"].connect(globalThis["audioCtx"].destination);
 		}
 	
 /*		
-		if (window["audioRec"] == undefined) {
-			window["audioRec"] = window["audioCtx"].createScriptProcessor(2048, 2, 2);
+		if (globalThis["audioRec"] == undefined) {
+			globalThis["audioRec"] = globalThis["audioCtx"].createScriptProcessor(2048, 2, 2);
 		}		
 
-        window["audioRec"].onaudioprocess = function(e) {
+        globalThis["audioRec"].onaudioprocess = function(e) {
 
 			let inputBuffer = e.inputBuffer;
 
 			//console.log( inputBuffer );
 
-			window["samplerate"] = inputBuffer.sampleRate;
+			globalThis["samplerate"] = inputBuffer.sampleRate;
 			let bufferSize = 2048;
 
-			window["channels"] = inputBuffer.numberOfChannels;
-			const dataArray = [ window["channels"] ];
+			globalThis["channels"] = inputBuffer.numberOfChannels;
+			const dataArray = [ globalThis["channels"] ];
 
-			for ( let i = 0; i < window["channels"]; i++ ) {
+			for ( let i = 0; i < globalThis["channels"]; i++ ) {
 				dataArray[i] = new Float64Array(bufferSize);
 			}
 
 			for ( let i = 0; i < bufferSize; i++ ) {
-				for ( let j = 0; j < window["channels"]; j++ ) {
+				for ( let j = 0; j < globalThis["channels"]; j++ ) {
 					dataArray[j][i] = inputBuffer.getChannelData(j)[i];
 				}
 			}
 
-			if ( window["queue"] != undefined ) {
-				const r = window["queue"].push( dataArray, bufferSize );
+			if ( globalThis["queue"] != undefined ) {
+				const r = globalThis["queue"].push( dataArray, bufferSize );
 				console.log( "queue.push: " + (( r == true ) ? "true" : "false") );
-				window["queue"].printAvailableReadAndWrite();
+				globalThis["queue"].printAvailableReadAndWrite();
 			}
 		};
 
-		window["audioSrc"].connect(window["audioRec"]);
-		window["audioRec"].connect(window["audioCtx"].destination);
+		globalThis["audioSrc"].connect(globalThis["audioRec"]);
+		globalThis["audioRec"].connect(globalThis["audioCtx"].destination);
 
 
-		window["audioCtx"].resume();
-		window["audioElement"].play();
+		globalThis["audioCtx"].resume();
+		globalThis["audioElement"].play();
 */
 
-		if (window["audioWorklet"] == undefined) 
+		if (globalThis["audioWorklet"] == undefined) 
 		{
-			window["audioWorklet"] = new AudioWorkletNode(window["audioCtx"], "radio-processor", {
+			globalThis["audioWorklet"] = new AudioWorkletNode(globalThis["audioCtx"], "radio-processor", {
 				processorOptions: {
-					queue: window["queue"],
-					instance: window["instance"],
+					queue: globalThis["queue"],
+					instance: globalThis["instance"],
 				},
 				numberOfInputs: 1,
 				numberOfOutputs: 1,
@@ -83,19 +83,19 @@ try {
 				channelInterpretation: "speakers"
 			});
 			
-			window["audioWorklet"].connect(window["audioCtx"].destination);
+			globalThis["audioWorklet"].connect(globalThis["audioCtx"].destination);
 		}
 
-		window["audioSrc"].connect(window["audioWorklet"]);
+		globalThis["audioSrc"].connect(globalThis["audioWorklet"]);
 /*
-		if (window["worker"] == undefined) 
+		if (globalThis["worker"] == undefined) 
 		{
-			window["worker"] = new Worker("./nk-radio/modules/radio/radio-worker.sync.mjs", {
+			globalThis["worker"] = new Worker("./nk-radio/modules/radio/radio-worker.sync.mjs", {
             	name: "internet - radio",
         	    type: "module",
     	    });
 
-			window["worker"].addEventListener('message', (event) => {
+			globalThis["worker"].addEventListener('message', (event) => {
 				if(event.data.status) {
 					switch (event.data.type) {
 						case 'terminate':
@@ -105,8 +105,8 @@ try {
 						default:
 							self.this.sharedArrayBuffer =  {
 								name: "internet - radio",
-								instance: window["instance"],
-								queue: window["queue"]
+								instance: globalThis["instance"],
+								queue: globalThis["queue"]
 							}
 	
 							document.dispatchEvent(new CustomEvent(`free-queue`, {
@@ -119,57 +119,57 @@ try {
 				}
 			}, { once: true })
 
-			window["worker"].postMessage({
+			globalThis["worker"].postMessage({
 				type: 'init',
 				data: {
-					instance: window["instance"],
-					queue: window["queue"],
+					instance: globalThis["instance"],
+					queue: globalThis["queue"],
 					sampleRate: 44100
 				}
 			});
 
 		}
 
-		window["worker"].onerror = (event) => {
+		globalThis["worker"].onerror = (event) => {
 			console.log('[main.js] Error from worker.js: ', event);
 		};
 	
-		if (window["audioWorklet"]) {
+		if (globalThis["audioWorklet"]) {
 
-			window["audioGain"] = window["audioCtx"].createGain();
-			window["audioAnalyser"] = window["audioCtx"].createAnalyser();
+			globalThis["audioGain"] = globalThis["audioCtx"].createGain();
+			globalThis["audioAnalyser"] = globalThis["audioCtx"].createAnalyser();
 
-			if ( window["audioAnalyser"] ) {
-				if ( window["waveform"] == undefined ) window["waveform"] = new Float32Array( window["audioAnalyser"].frequencyBinCount );
-				if ( window["waveform"] ) {
-//					window["audioAnalyser"].fftSize = 1024;
-					window["audioAnalyser"].getFloatTimeDomainData( window["waveform"] );
+			if ( globalThis["audioAnalyser"] ) {
+				if ( globalThis["waveform"] == undefined ) globalThis["waveform"] = new Float32Array( globalThis["audioAnalyser"].frequencyBinCount );
+				if ( globalThis["waveform"] ) {
+//					globalThis["audioAnalyser"].fftSize = 1024;
+					globalThis["audioAnalyser"].getFloatTimeDomainData( globalThis["waveform"] );
 				}
 			}
 	
-			window["audioGain"].connect( window["audioWorklet"] ).connect( window["audioAnalyser"] ).connect(window["audioCtx"].destination);
+			globalThis["audioGain"].connect( globalThis["audioWorklet"] ).connect( globalThis["audioAnalyser"] ).connect(globalThis["audioCtx"].destination);
 		}
 */
-		window["audioCtx"].resume();
-		window["audioElement"].play();
+		globalThis["audioCtx"].resume();
+		globalThis["audioElement"].play();
 		
     }
 
-    window["stopplayback"] = async function() {
-		window["isPlaybackInProgress"] = false;
-		window["render-buffer"] = undefined;
-		window["audioAnalyser"] = undefined;
-		window["audioElement"].pause();
-		//window["audioRec"].onaudioprocess = undefined;
+    globalThis["stopplayback"] = async function() {
+		globalThis["isPlaybackInProgress"] = false;
+		globalThis["render-buffer"] = undefined;
+		globalThis["audioAnalyser"] = undefined;
+		globalThis["audioElement"].pause();
+		//globalThis["audioRec"].onaudioprocess = undefined;
     }
 
 	/*
 
 	var draw = async function () {
 		requestAnimationFrame(draw);
-		if ( window["audioAnalyser"] != undefined ) {
-			await window["audioAnalyser"].getFloatTimeDomainData( window["waveform"] );
-			console.log( window["waveform"] );
+		if ( globalThis["audioAnalyser"] != undefined ) {
+			await globalThis["audioAnalyser"].getFloatTimeDomainData( globalThis["waveform"] );
+			console.log( globalThis["waveform"] );
 		}
 	}
 
