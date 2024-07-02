b1d103609d0e60389a171b3b616c6a1c5f4b3afc
vvzababurin <vvzababurin@gmail.com>
2024-07-02 15:50:20 +0300

    update

--- examples/src/app.mjs
+++ examples/src/app.mjs
@@ -4,26 +4,19 @@ try {
     globalThis["startplayback"] = async function()
     {
 		globalThis["isPlaybackInProgress"] = true;
-
 		if (globalThis["audioCtx"] == undefined) {
 			globalThis["audioCtx"] = new (globalThis.AudioContext || globalThis.webkitAudioContext)();
 		}
-
-		await globalThis["audioCtx"].audioWorklet.addModule("./nk-radio/modules/radio/radio-processor.mjs");
-		 
+		await globalThis["audioCtx"].audioWorklet.addModule("./nk-radio/modules/radio/radio-processor.mjs");		 
 		globalThis["audioCtx"].suspend();
 		globalThis["audioElement"] = globalThis.document.getElementById("audiostream");
-	
 		if (globalThis["audioSrc"] == undefined) {
 			globalThis["audioSrc"] = globalThis["audioCtx"].createMediaElementSource(globalThis["audioElement"]);
 		}
-
 		if (globalThis["audioSrc"]) {
 			globalThis["audioSrc"].connect(globalThis["audioCtx"].destination);
 		}	
-
-		if (globalThis["audioWorklet"] == undefined) 
-		{
+		if (globalThis["audioWorklet"] == undefined) {
 			globalThis["audioWorklet"] = new AudioWorkletNode(globalThis["audioCtx"], "radio-processor", {
 				processorOptions: {
 					queue: globalThis["queue"],
@@ -36,23 +29,19 @@ try {
 				channelCountMode: "max",
 				channelInterpretation: "speakers"
 			});
-			
 			globalThis["audioWorklet"].connect(globalThis["audioCtx"].destination);
 		}
-
 		globalThis["audioSrc"].connect(globalThis["audioWorklet"]);
 		globalThis["audioCtx"].resume();
 		globalThis["audioElement"].play();
 		
     }
-
     globalThis["stopplayback"] = async function() {
 		globalThis["isPlaybackInProgress"] = false;
 		globalThis["render-buffer"] = undefined;
 		globalThis["audioAnalyser"] = undefined;
 		globalThis["audioElement"].pause();
     }
-
 } 
 catch( e ) 
 {
